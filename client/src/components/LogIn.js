6f7ae7e549fe32a15797e0ed77223277421efedc
daniel wamsher <daniel.wamsher@gmail.com>
2022-01-12 09:54:13 -0800

    seperate mongoose uri for arch machine

---
+++ client/src/components/LogIn.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LogIn = () => {
+
+    function submit() {
+
+    }
+    return (
+        <div>
+        <form>
+        <label>username</label>
+        <input ></input>
+        <label>password</label>
+        <input></input>
+        <button onClick={submit}></button>
+        </form>
+        </div>
+    )
+}
+
+export default Log-In
